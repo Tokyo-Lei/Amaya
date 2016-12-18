800d8c2af010c3a7175569220d772f6a6c631760
DESKTOP-0391GP5\Kyomini <krosash@gmail.com>
2016-12-18 10:24:55 +0800

    修改控制器

--- app/home/controllers/HomeController.php
+++ app/home/controllers/HomeController.php
@@ -2,8 +2,8 @@
 /**
  * Amaya PHP Framework
  * User: OnoNaoki
- * Date: 2016/12/17
- * Time: 10:39
+ * Date: 2016/12/18
+ * Time: 12:18
  */
 
 class HomeController extends ShareControllers
@@ -12,27 +12,10 @@ class HomeController extends ShareControllers
     public function index()
     {
 
-
-//        require BASE_PATH.'/system/fcache.inc.php';
-//
-//        $cache = new FCache();
-       //$db = Article::first();
-
-
-
-//        $formdata = array();
-//        $password = 'admin';
-//        $formdata['factor'] = random_string('diy',5,'abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789');
-//        $formdata = md5(md5($password).$formdata['factor']);
-//
-//        echo $formdata;
-
         $db = Article::first();
-
         $loader = new Twig_Loader_Filesystem(VIEW_PATH);
         $twig = new Twig_Environment($loader, array('cache' => BASE_PATH.'/cache',));
         echo $twig->render('home.html', array('db' => $db));
-        //require VIEW_PATH.'home.php';
 
     }
 
